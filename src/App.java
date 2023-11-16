fe531d13895393891719ccc07f193167ae0a04fd
Amir <alternade1@gmail.com>
2023-11-15 20:08:03 -0800

    first commit

---
+++ src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int[] arr_1={1,2,3,4,5,6,7,8,9,10};
+        for(int i = 0; i < 10; i++ ){
+            System.out.println(arr_1[i]);
+        }
+        for(int y = 9; y >= 0; y--){
+            System.out.println(arr_1[y]);
+        };
+
+        }
+}
